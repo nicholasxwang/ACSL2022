fc5bc65b004ffea949ca410166ef4c95009614ef
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2022-11-18 15:49:50 -0800

    fixed bug with >10 bases

--- test1.py
+++ test1.py
@@ -17,10 +17,40 @@ import sys
 #  2. INTEGER base
 #  3. STRING start
 #
+conversions = {
+    "A": 10,
+    "B": 11,
+    "C": 12,
+    "D": 13,
+    "E": 14,
+    "F": 15,
+    "G": 16,
+}
+unconversions = {
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+    16: "G",
+}
+nextLetter = {
+    "A": "B",
+    "B": "C",
+    "C": "D",
+    "D": "E",
+    "E": "F",
+    "F": "G",
+}
+
 def findNextNumber(prev, base):
     prev = list(prev)
     for i in range(len(prev)):
-        prev[i] = int(prev[i])
+        if prev[i] not in conversions.keys():
+            prev[i] = int(prev[i])
+        else:
+            prev[i] = conversions[prev[i]]
     prev[-1] = prev[-1]+1
     for i in range(len(prev)-1, -1, -1):
         if (prev[i] == base):
@@ -29,27 +59,32 @@ def findNextNumber(prev, base):
                 prev.insert(0, 1)
             else:
                 prev[i-1] = prev[i-1]+1
-    return str(prev).replace("[", "").replace("]", "").replace(",", "").replace(" ", "")
+    # return str(prev).replace("[", "").replace("]", "").replace(",", "").replace(" ", "")
+    return_string = ""
+    for i in prev:
+        if (i < 10):
+            return_string += str(i)
+        else:
+            return_string += unconversions[i]
+    return return_string
 def findModeCount(num, base, start):
     count = 1
     generated = [str(start)]
     while count < num:
         generated.append(findNextNumber(generated[-1], base))
         count+=1
-    print(generated)
-    dictionary = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-    }
+    #print(generated)
+    dictionary = {}
+    for i in range(0, base):
+        dictionary[i] = 0
     for generate in generated:
         for character in str(generate):
-            dictionary[int(character)]+=1
+            # dictionary[int(character)]+=1
+            if character not in conversions.keys(): #not letter
+                dictionary[int(character)]+=1
+            else: #letter
+                dictionary[conversions[character]]+=1
+
     most = 0
     for value in dictionary.keys():
         if (dictionary[value] > dictionary[most]):
@@ -57,18 +92,18 @@ def findModeCount(num, base, start):
     return dictionary[most]
 
 if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    #
-    # num = int(input().strip())
-    #
-    # base = int(input().strip())
-    #
-    # start = input()
-    #
-    # result = findModeCount(num, base, start)
-    #
-    # fptr.write(str(result) + '\n')
-    #
-    # fptr.close()
-    findNextNumber("7", 8)
-    print(findModeCount(15, 8, 2))
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    num = int(input().strip())
+
+    base = int(input().strip())
+
+    start = input()
+
+    result = findModeCount(num, base, start)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+    # findNextNumber("7", 8)
+    # print(findModeCount(20, 12, "9AB"))
